009eaf9e6204ce5bff1ef80a6b9ebcb6573bb3fb
Basil Stotz <basil.stotz@gmail.com>
2022-07-18 07:46:31 +0200

    add project dir and move date there

--- project/bin/check-and-add-osmid.js
+++ project/bin/check-and-add-osmid.js
@@ -2,6 +2,8 @@
 
 const fs = require('fs');
 const { execSync } = require('child_process');
+const crypto = require('crypto')
+
 
 /*
 function read(name){
@@ -26,6 +28,7 @@ var maxDist;
 
 if(process.argv[2]){
     maxDist=process.argv[2];
+    if(maxDist<1.0)maxDist=1.0;
 }else{
     maxDist=1.0;
 }
@@ -36,15 +39,16 @@ function addMeta(geodata){
 
     var project=JSON.parse(geodata);
 
-    /*
-    var osm;
-    var path='openstreetmap/sorbusdomestica.geojson';
+  
+    var cache;
+    var path='./cache/id-cache.json';
     if (fs.existsSync(path)) {
-	osm=JSON.parse(fs.readFileSync(path));
+	cache=JSON.parse(fs.readFileSync(path));
     } else {
-	process.exit(0);
+	cache={}
     }
-    */
+
+    
     
     for (let i=0;i<project.length;i++){
 	
@@ -60,24 +64,39 @@ function addMeta(geodata){
 
 	let p=Gemeinde+'/'+Gebiet+' ';
 	let place=p.padEnd(61,'.');
+
+	let hash = crypto.createHash('md5').update(Gemeinde+Gebiet).digest("hex");
 	
 	let query="select id, latitude, longitude, (longitude - :lon)*(longitude - :lon)+(latitude - :lat)*(latitude - :lat) as dist from trees where dist != 'null' and dist >= 0.0  order by dist limit 1"
 	
         let cmd='sqlite-utils query openstreetmap/datasette/sorbusdomestica.sqlite \"'+query+'\" -p lon '+laenge+' -p lat '+breite;
 
-	let ans=[ { id: "", latitude: -999.9, longitude: -999.9, dist: -999.9 } ];
+	let dist;
+	
+	let ans=[ { id: "", latitude: -999.9, longitude: -999.9, dist: 999.9 } ];
 	//console.log( laenge+' '+breite+' '+Gemeinde+' '+Gebiet)
 	//console.log(cmd);
 	if((breite!='')&&(laenge!='')&&(Gemeinde!='')){
 	    if((breite.indexOf(' ')==-1)&&(laenge.indexOf(' ')==-1)){
-		try {
-		    ans=JSON.parse(execSync(cmd,[],{ encoding: 'utf8' }));
-		    //console.log(JSON.stringify(ans,"",2));
+		//get data
+		if(cache[hash]){
+		    ans=JSON.parse(JSON.stringify(cache[hash]));
+		}else{
+		    try {
+			ans=JSON.parse(execSync(cmd,[],{ encoding: 'utf8' }));
+			//console.log(JSON.stringify(ans,"",2));
+		    }
+		    catch {
+			process.stderr.write('error: (länge='+laenge+',breite='+breite+'): '+Gemeinde+'/'+Gebiet+'\n')
+		    }
+		    let dister=Math.round(Math.sqrt(ans[0].dist)*100000.0*100.0)/100.0;
+		    if(dister<0.9999){
+			//process.stderr.write(JSON.stringify(ans,null,2)+'\n');
+			cache[hash] = JSON.parse(JSON.stringify(ans));
+			//cache[hash] = Object.create(ans);
+		    }
 		}
-		catch {
-		    process.stderr.write('error: (länge='+laenge+',breite='+breite+'): '+Gemeinde+'/'+Gebiet+'\n')
-		}
-		let dist=Math.round(Math.sqrt(ans[0].dist)*100000.0*100.0)/100.0;
+		dist=Math.round(Math.sqrt(ans[0].dist)*100000.0*100.0)/100.0;
 		if(dist>maxDist){
 		    if(fehlend==''){
 			ans=[ { id: "", latitude: -999.9, longitude: -999.9, dist: dist } ];
@@ -91,17 +110,67 @@ function addMeta(geodata){
 	    }else{
 		process.stderr.write(place+' ==> länge=\"'+laenge+'\", breite=\"'+breite+'\"\n')
 		ans=[ { id: "", latitude: -999.9, longitude: -999.9, dist: -999.9 } ];
-	    }  
-		project[i]['id']=ans[0].id;
-		project[i]['latitude']=ans[0].latitude;
-		project[i]['longitude']=ans[0].longitude;
-	        project[i]['dist']=ans[0].dist;
+	    }
+
+	    let autochton=''
+	    let natural=''
+	    
+            switch(Natuerlich){
+	    /*
+	    case '':
+	    case ' ':
+	    case '-':
+		autochton='';
+		natural='';
+		break;
+            */
+	    case 'N':
+		autochton='yes';
+		natural='yes'
+		break;
+	    case 'N ?':
+	    case 'N?':
+	    case 'N/P?':
+	    case 'P?':
+	    case 'P  (?)':
+		autochton='maybe';
+		natural='maybe';
+		break;
+	    case 'P (von N)':
+		autochton='yes';
+		natural='no'
+		break;
+	    case 'N (tot)':
+	    case 'N (tot) ':
+	    case 'P (tot)':
+	    case 'P? (tot)':
+		autochton='';
+		natural='';
+		break;
+	    default:
+		if(Natuerlich.indexOf('P')==0){
+		    autochton='no';
+		    natural='no';
+		}
+		break;
+	    }
+	    
+	    project[i]['id']=ans[0].id;
+	    project[i]['plantation_year']='';
+	    project[i]['hash']=hash;
+	    project[i]['autochton']=autochton;
+	    project[i]['natural']=natural;
+	    project[i]['latitude']=ans[0].latitude;
+	    project[i]['longitude']=ans[0].longitude;
+	    project[i]['dist']=ans[0].dist;
+	    
         }else{
-	    delete project[i];
+	    project.splice(i,1);
 	    process.stderr.write('     leere zeile gelöscht\n')
 	}
     }
-    
+
+    fs.writeFileSync(path,JSON.stringify(cache,null,2),{encoding:'utf8', flag:'w'});
     process.stdout.write(JSON.stringify(project,null,2)+'\n');
 }
 
